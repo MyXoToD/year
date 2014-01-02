22e7a2faec0d548805b8dd56d36d729798bed609
Christian Fei <info@christian-fei.com>
2014-01-02 15:04:29 +0100

    install, link to prject

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ PWD=`pwd`
 sudo rm /usr/local/bin/year
 sudo ln -s "$PWD/year.sh" /usr/local/bin/year
 echo "created executable in /usr/local/bin/year"
+rm ~/year.txt
 touch ~/year.txt
-echo "created ~/year.txt"
+rm "$PWD/year.txt"
+ln -s ~/year.txt "$PWD/year.txt"
+echo "created ~/year.txt and link to git directory"

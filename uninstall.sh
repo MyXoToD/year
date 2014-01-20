fcfdebd6f5d43357da6ee59389e05fad4c496aca
MyXoToD <webmaster@myxotod.de>
2014-01-20 12:34:23 +0100

    Remove autocompletion on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,4 +4,6 @@ sudo rm /usr/local/bin/year
 echo "Deleted: /usr/local/bin/year"
 sudo rm ~/year.txt
 echo "Deleted: ~/year.txt"
+sudo rm /etc/bash_completion.d/year
+echo "Removed autocompletition"
 echo "Successfully uninstalled year."
